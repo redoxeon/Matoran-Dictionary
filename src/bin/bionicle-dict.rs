60014e42ed39d2c745495e4853880d3a6660bc46
Mike Harding <redoxeon@mharding.dev>
2020-03-03 23:25:40 -0700

    added dictionary struct

--- src/bin/bionicle-dict.rs
+++ src/bin/bionicle-dict.rs
@@ -1,16 +1,18 @@
 extern crate clap;
 
-use clap::{crate_authors, crate_description, crate_version, App, Arg};
+// use clap::{crate_authors, crate_description, crate_version, App, Arg};
 
 fn main() {
-    let config = App::new("bionicle-dict")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .arg(
-            Arg::with_name("DEFINITION")
-                .help("Help message in the works... sorry.")
-                .index(1),
-        )
-        .get_matches();
+    // not ready for this yet
+
+    // let config = App::new("bionicle-dict")
+    //     .version(crate_version!())
+    //     .author(crate_authors!())
+    //     .about(crate_description!())
+    //     .arg(
+    //         Arg::with_name("DEFINITION")
+    //             .help("Help message in the works... sorry.")
+    //             .index(1),
+    //     )
+    //     .get_matches();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,82 @@
+use std::collections::HashMap;
 use std::fmt;
+
+#[derive(Debug)]
+pub struct MatoranDictionary {
+    entries: HashMap<String, MatoranDictionaryEntry>,
+}
 
 #[derive(Debug)]
 pub struct MatoranDictionaryEntry {
     entry: String,
-    word_type: String,
-    ix_kind: AffixKind,
+    typing: WordType,
     pronounciation: String,
     definition: String,
     etymology: String,
 }
 
-pub enum AffixKind {
-    Prefix,
-    Suffix,
+impl MatoranDictionaryEntry {
+    pub fn new(
+        entr: String,
+        word_type: WordType,
+        pronounce: String,
+        def: String,
+        etym: String,
+    ) -> Self {
+        MatoranDictionaryEntry {
+            entry: entr,
+            typing: word_type,
+            pronounciation: pronounce,
+            definition: def,
+            etymology: etym,
+        }
+    }
+}
+
+pub enum WordType {
+    Adjective,
+    AdjectiveCompound,
+    Adverb,
+    Affix,
+    Noun,
+    NounCompound,
+    Particle,
+    ParticleCompound,
+    Pronoun,
+    Stem,
+    Verb,
+    VerbalNoun,
+    VerbalNounCompound,
     None,
 }
 
-impl std::fmt::Debug for AffixKind {
+impl std::fmt::Debug for WordType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            AffixKind::Prefix => write!(f, "Prefix"),
-            AffixKind::Suffix => write!(f, "Suffix"),
-            _ => write!(f, "Neither"),
+            WordType::Adjective => write!(f, "Adjective"),
+            WordType::AdjectiveCompound => write!(f, "AdjectiveCompound"),
+            WordType::Adverb => write!(f, "Adverb"),
+            WordType::Affix => write!(f, "Affix"),
+            WordType::Noun => write!(f, "Noun"),
+            WordType::NounCompound => write!(f, "NounCompound"),
+            WordType::Particle => write!(f, "Particle"),
+            WordType::ParticleCompound => write!(f, "ParticleCompound"),
+            WordType::Pronoun => write!(f, "Pronoun"),
+            WordType::Stem => write!(f, "Stem"),
+            WordType::Verb => write!(f, "Verb"),
+            WordType::VerbalNoun => write!(f, "VerbalNoun"),
+            WordType::VerbalNounCompound => write!(f, "VerbalNounCompound"),
+            _ => write!(f, "None"),
         }
+    }
+}
+
+impl MatoranDictionary {
+    pub fn new() -> Self {
+        MatoranDictionary {
+            entries: HashMap::new(),
+        }
+        // TODO: Either read in the whole thing when you start a new one or hard code the whole thing in here.
     }
 }
 
